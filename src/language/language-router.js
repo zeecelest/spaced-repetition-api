142516ba2efbcbbaba6163ee8c1bf20b2242744e
Zee Celest <zeecelest@gmail.com>
2020-01-02 15:08:51 -0500

    token error

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -2,7 +2,8 @@ const express = require('express')
 const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 
-const languageRouter = express.Router()
+const languageRouter = express.Router();
+const jsonBodyParser = express.json();
 
 languageRouter
   .use(requireAuth)
@@ -10,20 +11,20 @@ languageRouter
     try {
       const language = await LanguageService.getUsersLanguage(
         req.app.get('db'),
-        req.word.id,
-      )
+        req.params.id
+      );
 
       if (!language)
         return res.status(404).json({
           error: `You don't have any languages`,
-        })
+        });
 
-      req.language = language
-      next()
+      req.language = language;
+      next();
     } catch (error) {
       next(error)
     }
-  })
+  });
 
 languageRouter
   .get('/', async (req, res, next) => {
@@ -33,29 +34,56 @@ languageRouter
       //and returning the translated word.
       const words = await LanguageService.getLanguageWords(
         req.app.get('db'),
-        req.language.id,
-      )
+        req.language.id
+      );
 
       res.json({
         language: req.language,
         words,
-      })
-      next()
+      });
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
-  })
+  });
 
 languageRouter
   .get('/head', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
-  })
+  try {
+    const head = await LanguageService.getLanguageHead(req.app.get('db'),
+    req.language.id);
+    // res.send('implement me!')
+    res.json({
+      nextWord: head.original,
+      totalScore: head.total_score,
+      wordCorrectCount: head.correct_count,
+      wordIncorrectCount: head.incorrect_count
+    });
+    next();
+} catch (error) {
+    next(error);
+  }
+});
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
-  })
+  .post('/guess', jsonBodyParser, async (req, res, next) => {
+   try {
+     const { guess, original, language_id } = req.body;
+     if (!guess) {
+       return res.status(400).json({ error: "Missing 'guess' in request body" });
+     }
+     const translation = await LanguageService.getWordTranslation (req.app.get('db'), original);
+     if ( guess === translation.translation) {
+        const word = await LanguageService.handleCorrectCount(req.app.get('db'), language_id, original);
+        res.json({ answer: guess, isCorrect: true, ...word });
+     } else {
+       const word = await LanguageService.handleIncorrectCount(req.app.get('db'), language_id, original);
+       res.json({ answer: guess, isCorrect: false, ...word});
+     }
+     next();
+   } catch (error) {
+     next(error);
+   }
+});
 
-module.exports = languageRouter
+module.exports = languageRouter;
